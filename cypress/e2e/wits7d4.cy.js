7e26f5a5f686e318470d68bbd171074671593624
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-01-23 18:13:55 +0300

    ilk test eklendi

--- cypress/e2e/wits7d4.cy.js
+++ cypress/e2e/wits7d4.cy.js
@@ -2,9 +2,17 @@ describe('template spec', () => {
   beforeEach(()=>{
     cy.visit('http://localhost:5173/')
   })
-  it('passes', () => {
-    
+  it('başarılı formda success sayfası açılıyor', () => {
+    cy.get(`input[name="email"]`).type("abcd@email.com.tr");
+    cy.get(`input[name="password"]`).type("abcd@email.com.tr");
+    cy.get(`input[name="terms"]`).check();
+    cy.get(`button`).click();
+    cy.contains("Successful login!")
   })
 
+  it.only("email yanlış girdin",()=>{
+
+
+  })
 
 })
